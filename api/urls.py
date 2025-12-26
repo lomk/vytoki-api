c322a63061942268bf81f544eae0dd6a600cf4f3
Igor Materynko <igor@materynko.com>
2025-12-26 22:21:46 +0200

    urls update

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("health/", views.health),
-    path("sections/", views.sections_list),
-    path("sections/<slug:slug>/", views.section_detail),
-    path("sections/<slug:section_slug>/articles/", views.articles_list),
-    path("sections/<slug:section_slug>/articles/<int:article_id>/", views.article_detail),
+    path("services/", views.service_list),
+    path("services/<slug:slug>/", views.service_detail),
+    path("services/<slug:service_slug>/articles/", views.articles_list),
+    path("services/<slug:service_slug>/articles/<int:article_id>/", views.article_detail),
     path("contacts/", views.contacts),
 ]
